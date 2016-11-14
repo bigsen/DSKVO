dcb39a16975b09bbcafdb109fd487ab58662605f
zhangdasen <zhangdasen@youku.com>
2016-11-14 11:46:34 +0800

    更新添加observer判断

--- DSKVO/NSObject+DSKVO.m
+++ DSKVO/NSObject+DSKVO.m
@@ -23,12 +23,12 @@
     SEL myRemoveSel = @selector(removeDasen:forKeyPath:);
     SEL addSel = @selector(addObserver:forKeyPath:options:context:);
     SEL myaddSel = @selector(addDasen:forKeyPath:options:context:);
-
+    
     Method systemRemoveMethod = class_getClassMethod([self class],removeSel);
     Method DasenRemoveMethod = class_getClassMethod([self class], myRemoveSel);
     Method systemAddMethod = class_getClassMethod([self class],addSel);
     Method DasenAddMethod = class_getClassMethod([self class], myaddSel);
-
+    
     method_exchangeImplementations(systemRemoveMethod, DasenRemoveMethod);
     method_exchangeImplementations(systemAddMethod, DasenAddMethod);
 }
@@ -95,7 +95,7 @@
 // 交换后的方法
 - (void)removeDasen:(NSObject *)observer forKeyPath:(NSString *)keyPath
 {
-    if ([self observerKeyPath:keyPath]) {
+    if ([self observerKeyPath:keyPath observer:observer]) {
         [self removeDasen:observer forKeyPath:keyPath];
     }
 }
@@ -103,29 +103,27 @@
 // 交换后的方法
 - (void)addDasen:(NSObject *)observer forKeyPath:(NSString *)keyPath options:(NSKeyValueObservingOptions)options context:(void *)context
 {
-    if (![self observerKeyPath:keyPath]) {
+    if (![self observerKeyPath:keyPath observer:observer]) {
         [self addDasen:observer forKeyPath:keyPath options:options context:context];
     }
 }
 
 
 // 进行检索获取Key
-- (BOOL)observerKeyPath:(NSString *)key
+- (BOOL)observerKeyPath:(NSString *)key observer:(id )observer
 {
     id info = self.observationInfo;
     NSArray *array = [info valueForKey:@"_observances"];
     for (id objc in array) {
         id Properties = [objc valueForKeyPath:@"_property"];
+        id newObserver = [objc valueForKeyPath:@"_observer"];
+        
         NSString *keyPath = [Properties valueForKeyPath:@"_keyPath"];
-        if ([key isEqualToString:keyPath]) {
+        if ([key isEqualToString:keyPath] && [newObserver isEqual:observer]) {
             return YES;
         }
     }
     return NO;
 }
-
-
-
-
 
 @end
